33fa7ec6f1ac8aa408607dbfd0b0df8fb0320353
igor <fabrisigor01@gmail.com>
2022-06-14 11:12:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+const animals = {
+    chicken: "Gertrude",
+    pig: "Henry",
+    sheep: "Melvin",
+    rabbit: "Billy"
+}
+let animals1 = Object.keys(animals) // [ 'chicken', 'pig', 'sheep', 'rabbit' ]
+
+let names = Object.values(animals) //[ 'Gertrude', 'Henry', 'Melvin', 'Billy' ]
+
+
+for (let i = 0; i < animals1.length; i++) {
+    let randomIndex = Math.floor(Math.random() * names.length)
+    let randomName = names[randomIndex]
+    let randomAnimal = animals1[i]
+    console.log(`${randomName} is a ${randomAnimal}`)
+  }
+  
