e8ee28fc61cfb6b770c998b5d74cc5609380d88c
igor <fabrisigor01@gmail.com>
2022-06-14 11:23:15 +0200

    correction loop

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ let animals1 = Object.keys(animals) // [ 'chicken', 'pig', 'sheep', 'rabbit' ]
 let names = Object.values(animals) //[ 'Gertrude', 'Henry', 'Melvin', 'Billy' ]
 
 
-for (let i = 0; i < animals1.length; i++) {
-    let randomIndex = Math.floor(Math.random() * names.length)
-    let randomName = names[randomIndex]
-    let randomAnimal = animals1[i]
-    console.log(`${randomName} is a ${randomAnimal}`)
-  }
+// for (let i = 0; i < animals1.length; i++) {
+//     let randomIndex = Math.floor(Math.random() * names.length)
+//     let randomName = names[randomIndex]
+//     let randomAnimal = animals1[i]
+//     console.log(`${randomName} is a ${randomAnimal}`)
+//   }
   
+
+for (let animal of animals1) {
+    let randomAnimal = Math.floor(Math.random() * names.length)
+    let randomName = names[randomAnimal]
+    console.log(`${randomName} is a ${animal}`)
+  }
+
+ 
